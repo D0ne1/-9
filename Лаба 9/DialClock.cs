30b08fb5249f8e8e82512975a300e4802c29e3c5
Daniil Panfilov <dpanfilov12@gmail.com>
2024-02-01 09:40:12 +0500

    Add project files.

---
+++ Лаба 9/DialClock.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace Лаба_9
+{
+    internal class DialClock
+    {
+        private int hours;
+        private int minutes;
+        static int count = 0;
+        public DialClock() // если конструктор пустой
+        {
+            this.Hours = 0;
+            this.Minutes = 0;
+            count++;
+        }
+        public DialClock(int hours, int minutes) // конструктор с параметрами
+        {
+            this.Hours = hours;
+            this.Minutes = minutes;
+            count++;
+        }
+        public DialClock(DialClock clock) // конструктор копирования
+        {
+            Hours = clock.Hours;
+            Minutes = clock.Minutes;
+            count++;
+        }
+
+        public int Hours
+        {
+            get
+            {
+
+                return hours;
+            }
+            set
+            {
+                if (value < 0) value = 0;
+                if (value - 12 >= 0) value = value - 12;
+                else hours = value;
+            }
+        }
+        public int Minutes
+        {
+            get
+            {
+                return minutes;
+            }
+            set
+            {
+                if (value < 0 || value == 60) { value = 0; }
+                //if (value > 60) { Console.WriteLine("еррор"); }
+                minutes = value;
+            }
+        }
+        public double GetCorner() //вычисление угла между часовой и минутной стрелкой
+        {
+            double hr = hours; //вспомогательная переменная типа double
+            double bias; //смещение по часовой стрелке
+            double persentage; //процент смещения 
+            double angle; //конечный угол
+            int totalcountminutes = 60; //количество  минут на механических часах
+            int totalcountminutesinhour = 5; //количество рисок в часе на механических часах
+            int degreeminuts = 6; //количество градусов за 1 минуту 
+                
+
+            persentage = totalcountminutes * 100 / minutes;
+            bias = totalcountminutesinhour * degreeminuts * persentage / 100;
+
+            hr += bias;
+
+            hr *= degreeminuts;
+            minutes *= degreeminuts;
+            angle = Math.Abs(hr - minutes);
+            return angle;
+            
+        }
+
+        public void Show() //вывод на экран
+        {
+            Console.WriteLine($"Часы: {hours}, минуты: {minutes}.");
+        }
+
+        public static void ShowStatic(DialClock clock) // вывод на экран (статическая функция)
+        {
+            Console.WriteLine($"Часы: {clock.hours}, минуты: {clock.minutes}.");
+        }
+        public static int GetCount() { return count; }
+    }
+}
---
+++ Лаба 9/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаба_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double angle1;
+            double angle2;
+            double angle3;
+
+            DialClock clock1 = new DialClock(); // конструктор без параметров
+            clock1.Hours = 12;
+            clock1.Minutes = 20;
+            clock1.Show();
+
+            DialClock clock2 = new DialClock(12,20); // конструктор с параметрами
+            clock2.Show();
+
+            DialClock clock3 = new DialClock(clock2); // конструктор копирования
+            clock2.Show();
+
+            DialClock.ShowStatic(clock1);
+
+            angle1 = clock1.GetCorner();
+            Console.WriteLine(angle1);
+
+            angle2 = clock2.GetCorner();
+            Console.WriteLine(angle2);
+
+            angle3 = clock3.GetCorner();
+            Console.WriteLine(angle3);
+
+            Console.WriteLine(DialClock.GetCount());
+        }
+    }
+}
